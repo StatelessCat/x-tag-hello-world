593be43eda68e5709623ad26a1f451b338002f1b
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2015-06-21 12:24:32 +0200

    small stub for defining customs elements using x-tag

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # x-tag-hello-world
+
+# get dependencies
+`bower install`
+
+# Demo
+## get demo dependencies
+`npm install -d`
+
+## run the demo
+`grunt connect`
---
+++ my-component.js
@@ -0,0 +1,23 @@
+(function(){
+
+  xtag.register('x-my-component', {
+    lifecycle: {
+      created: function() {
+        console.log("created");
+      },
+      inserted: function() {},
+      removed: function() {},
+      attributeChanged: function() {}
+    },
+    events: {
+
+    },
+    accessors: {
+
+    },
+    methods: {
+
+    }
+  });
+
+})();
